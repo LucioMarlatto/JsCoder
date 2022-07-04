aa98d07029f340efb189512a586d38af80f0d32c
Lucio <luciomarlatto0306@gmail.com>
2022-07-04 18:55:37 -0300

    Desafio Interactuando con HTML

---
+++ js/main.js
@@ -0,0 +1,19 @@
+    alert ('Bienvenidos a Karma')
+
+
+const mostrarProductos = () => {
+    const contenedor = document.getElementById("producto-contenedor");
+
+    productos.forEach( producto => {
+        const div = document.createElement("div");
+        div.classList.add("card");
+        div.innerHTML = `<div class="card-image">
+                            <img src=${producto.img}>
+                            <span class="card-title">${producto.nombre}</span>
+                        </div>
+                        `
+        contenedor.appendChild(div);
+    });
+};
+
+mostrarProductos();
---
+++ js/stock.js
@@ -0,0 +1,80 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Zapatilla",
+        desc: "Altas zapas",
+        precio: 1200,
+        img: 'img/zapatilla1.png',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Zapatilla",
+        desc: "Altas zapas",
+        precio: 500,
+        img: 'img/zapatilla2.png',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Zapatilla",
+        desc: "Altas zapas",
+        precio: 900,
+        img: 'img/zapatilla3.png',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Zapatilla",
+        desc: "Altas zapas",
+        precio: 1600,
+        img: 'img/zapatilla4.png',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Zapatilla",
+        desc: "Altas zapas",
+        precio: 1200,
+        img: 'img/zapatilla5.png',
+        cantidad:1,
+    },
+
+    {
+        id: 6,
+        nombre: "Zapatilla",
+        desc: "Altas zapas",
+        precio: 500,
+        img: 'img/zapatilla6.png',
+        cantidad:1,
+    },
+    {
+        id: 7,
+        nombre: "Zapatilla",
+        desc: "Altas zapas",
+        precio: 500,
+        img: 'img/zapatilla7.png',
+        cantidad:1,
+    },
+    {
+        id: 8,
+        nombre: "Zapatilla",
+        desc: "Altas zapas",
+        precio: 500,
+        img: 'img/zapatilla8.png',
+        cantidad:1
+    },
+    {
+        id: 9,
+        nombre: "Zapatilla",
+        desc: "Altas zapas",
+        precio: 500,
+        img: 'img/zapatilla9.png',
+        cantidad:1
+    }
+];
