5d12d482a6cec998b65e3e57f3e408b71c8f8f28
Lucio <luciomarlatto0306@gmail.com>
2022-07-06 18:25:00 -0300

    Agregando DarkMode

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
     alert ('Bienvenidos a Karma')
 
 
-const mostrarProductos = () => {
-    const contenedor = document.getElementById("producto-contenedor");
+// const mostrarProductos = () => {
+//     const contenedor = document.getElementById("producto-contenedor");
 
-    productos.forEach( producto => {
-        const div = document.createElement("div");
-        div.classList.add("card");
-        div.innerHTML = `<div class="card-image">
-                            <img src=${producto.img}>
-                            <span class="card-title">${producto.nombre}</span>
-                        </div>
-                        `
-        contenedor.appendChild(div);
-    });
-};
+//     productos.forEach( producto => {
+//         const div = document.createElement("div");
+//         div.classList.add("card");
+//         div.innerHTML = `<div class="card-image">
+//                             <img src=${producto.img}>
+//                             <span class="card-title">${producto.nombre}</span>
+//                         </div>
+//                         `
+//         contenedor.appendChild(div);
+//     });
+// };
 
-mostrarProductos();
+// mostrarProductos();
+
+
+function cambiarModo() { 
+    const cuerpoweb = document.body; 
+    cuerpoweb.classList.toggle("darkmode"); 
+}
+
